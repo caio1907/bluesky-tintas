ab3c6632b50c190fcdc84b086e5cbf26a21ad04b
carlosefl <art97496@gmail.com>
2023-04-28 16:26:45 -0300

    adição dos botões e ajuste na identação

--- src/screens/Cadastros/index.tsx
+++ src/screens/Cadastros/index.tsx
@@ -1,36 +1,46 @@
 import React from 'react';
 import useStyles from './styles';
 import { colors } from '@mui/material';
-import {TextField} from '@mui/material';
+import { Box, Grid, TextField, Button, FormControl } from '@mui/material';
 
 const Cadastros: React.FC = () => {
-    const classes = useStyles();
+  return (
+    <div>
+      <h1 style={{ color: '#00264d' }}>Cadastro Itens:</h1>
 
+      <Box mb={2} />
 
-    return (
-        <div>
-            <h1 style={{color:'#00264d'}}>Cadastro Itens</h1>
+      <Grid container columnSpacing={17} direction="row" justifyContent="space-evenly" alignItems="flex-start">
+        <TextField id="Cod" label="Código" variant="outlined" />
+        <TextField id="Nome" label="Nome*" variant="outlined" style={{ width: 340 }} />
+      </Grid>
 
-            <TextField id="Cod" label="Código" variant="outlined" />
-            <TextField id="Nome" label="Nome*" variant="outlined" />
-            <TextField id="Cor" label="Cor" variant="outlined" />
-            <TextField id="Tipo" label="Tipo" variant="outlined" />
-            <TextField id="Forne" label="Fornecedor" variant="outlined" />
-            <TextField multiline maxRows={4} id="Descri" label="Descrição" variant="standard"/>
-        </div>
-    );
+      <Box mb={2} />
 
+      <Grid container direction="row" justifyContent="space-evenly" alignItems="center" columnSpacing={7}>
+        <TextField id="Cor" label="Cor" variant="outlined" style={{ width: 159 }} />
+        <TextField id="Tipo" label="Tipo" variant="outlined" />
+        <TextField id="Forne" label="Fornecedor" variant="outlined" />
+      </Grid>
 
+      <Box mb={2} />
 
+      <FormControl style={{ width: 354 }}>
+        <TextField multiline maxRows={4} fullWidth id="Descri" label="Descrição" variant="standard" />
+      </FormControl>
 
+      <Box mb={6} />
 
-
-
-
-
-
-
-
-
+      <Grid container direction="row-reverse" justifyContent="space-between" alignItems="flex-end">
+        <Button variant="contained" color="success">
+          confirmar
+        </Button>
+        <Button variant="contained" color="error">
+          cancelar
+        </Button>
+      </Grid>
+    </div>
+  );
 };
+
 export default Cadastros;
