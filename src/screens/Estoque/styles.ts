cc72dcd759e46d3ba5c5977a478d1dbb4a92e5b8
Caio Lima <caio.1907@hotmail.com>
2023-05-03 14:22:25 -0300

    feature: Footer section has been created

--- src/screens/Estoque/styles.ts
+++ src/screens/Estoque/styles.ts
@@ -1,21 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
-  h1:{
-    color: '#00264d'
-},
-  tabela:{
-    width:'800px',
-
-  },
-  botao:{
-    width:'55px'
-  },
-  // o botãoler é o botão do ler mais :3
-  botaoler:{
-    right:'62px',
-    top:'6px'
-  },
   minQuantityRow: {
     backgroundColor: theme.palette.error.main,
     color: '#FFF'
